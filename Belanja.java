bfdd2d084d80ff16f5ae1031ae0260e8e346d318
erdynufayudhatama <63667550+erdynufayudhatama@users.noreply.github.com>
2020-04-15 18:08:57 +0800

    Add files via upload

---
+++ Belanja.java
@@ -0,0 +1,27 @@
+package id.nufa.aplikasidaftarbelanja;
+
+public class Belanja {
+    private String name;
+    private int quantity;
+
+    public Belanja(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
